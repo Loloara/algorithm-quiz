3eb8101507de94e325e61a2990ef2b325bedfa9b
loloara <addy94@naver.com>
2018-10-25 20:52:07 +1000

    10973 solved

---
+++ baekjoon/브루트포스/10973.cpp
@@ -0,0 +1,25 @@
+//10973 이전 순열 https://www.acmicpc.net/problem/10973
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	if (prev_permutation(arr.begin(), arr.end())) {
+		for (int i = 0; i < n; i++) {
+			cout << arr[i] << ' ';
+		}
+	}
+	else
+		cout << -1;
+
+	return 0;
+}
