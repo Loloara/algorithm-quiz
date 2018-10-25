da339501a7d6d595ff8aab56d943bcb52c973cd0
loloara <addy94@naver.com>
2018-10-25 20:51:50 +1000

    10972 solved

---
+++ baekjoon/브루트포스/10972.cpp
@@ -0,0 +1,25 @@
+//10972 다음 순열 https://www.acmicpc.net/problem/10972
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	if (next_permutation(arr.begin(), arr.end())) {
+		for (int i = 0; i < n; i++) {
+			cout << arr[i] << ' ';
+		}
+	}
+	else
+		cout << -1;
+
+	return 0;
+}
