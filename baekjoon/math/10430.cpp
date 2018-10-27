da3c0ed30a50f30ac826abb5dced042308f8e4e7
loloara <addy94@naver.com>
2018-10-27 20:31:01 +1000

    10430 solved

---
+++ baekjoon/math/10430.cpp
@@ -0,0 +1,14 @@
+//10430 ³ª¸ÓÁö https://www.acmicpc.net/problem/10430
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << (a + b) % c << '\n';
+	cout << (a % c + b % c) % c << '\n';
+	cout << (a * b) % c << '\n';
+	cout << (a % c * b % c) % c;
+
+	return 0;
+}
