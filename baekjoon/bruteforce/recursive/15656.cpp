dd25732c6c948e7c2c1b4c416404a0946628ae3e
loloara <addy94@naver.com>
2018-10-30 17:25:35 +1000

    15656 solved

---
+++ baekjoon/bruteforce/recursive/15656.cpp
@@ -0,0 +1,38 @@
+//15656 N과 M(7) https://www.acmicpc.net/problem/15656
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int arr[8];
+vector<int> nArr;
+void go(int index, int n, int m);
+
+int main() {
+	int n, m, num;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		nArr.push_back(num);
+	}
+	sort(nArr.begin(), nArr.end());
+	go(0, n, m);
+
+	return 0;
+}
+
+void go(int index, int n, int m) {
+	//정답인 경우
+	if (index == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	//다음 경우
+	for (int i = 0; i < n; i++) {
+		arr[index] = nArr[i];
+		go(index + 1, n, m);
+	}
+}
