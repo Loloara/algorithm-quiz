ab87002c14bc1ecdb9533fb2f2f2c2a12ea48400
loloara <addy94@naver.com>
2018-11-04 22:24:12 +1000

    1697 solved

---
+++ baekjoon/graph/bfs/1697.cpp
@@ -0,0 +1,45 @@
+//1697 ¼û¹Ù²ÀÁú https://www.acmicpc.net/problem/1697
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int N, K;
+	cin >> N >> K;
+
+	queue<int> Q;
+	bool chk[100001] = {};
+	int dist[100001] = { 0 };
+	Q.push(N);
+	chk[N] = true;
+
+	while (!Q.empty()) {
+		int now = Q.front();
+		Q.pop();
+		
+		int next = now + 1;
+		if (!chk[next] && next >= 0 && next <= 100000) {
+			Q.push(next);
+			chk[next] = true;
+			dist[next] = dist[now] + 1;
+		}
+
+		next = now - 1;
+		if (!chk[next] && next >= 0 && next <= 100000) {
+			Q.push(next);
+			chk[next] = true;
+			dist[next] = dist[now] + 1;
+		}
+
+		next = now * 2;
+		if (!chk[next] && next >= 0 && next <= 100000) {
+			Q.push(next);
+			chk[next] = true;
+			dist[next] = dist[now] + 1;
+		}
+	}
+
+	cout << dist[K];
+
+	return 0;
+}
