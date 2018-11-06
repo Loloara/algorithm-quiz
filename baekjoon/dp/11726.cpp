768ddffeae10787244a6c2d0c51f9fe1cc7341b8
loloara <addy94@naver.com>
2018-11-06 20:35:57 +1000

    11726 solved

---
+++ baekjoon/dp/11726.cpp
@@ -0,0 +1,24 @@
+//11726 2xn ≈∏¿œ∏µ https://www.acmicpc.net/problem/11726
+#include <iostream>
+using namespace std;
+
+int mem[1001] = { 0 };
+int go(int index);
+
+int main() {
+	int n;
+	mem[1] = 1;
+	mem[2] = 2;
+
+	cin >> n;
+	cout << go(n);
+
+	return 0;
+}
+
+int go(int index) {
+	if (mem[index] != 0) return mem[index];
+	mem[index] = (go(index - 1) % 10007 + go(index - 2) % 10007) % 10007;
+
+	return mem[index];
+}
