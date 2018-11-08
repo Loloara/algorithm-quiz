e38f23c173dfdf556b1709d74b61e03265b522ef
loloara <addy94@naver.com>
2018-11-08 14:16:27 +1000

    2193 solved

---
+++ baekjoon/dp/2193.cpp
@@ -0,0 +1,28 @@
+//2193 ÀÌÄ£¼ö https://www.acmicpc.net/problem/2193
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+long long mem[91][2];
+long long go(int index, int last);
+
+int main() {
+	int N;
+	memset(mem, -1, sizeof(mem));
+	mem[1][0] = 0;
+	mem[1][1] = 1;
+
+	cin >> N;
+	cout << go(N, 0) + go(N, 1);
+
+	return 0;
+}
+long long go(int index, int last) {
+	if (mem[index][last] != -1) return mem[index][last];
+	if (last == 1)
+		mem[index][last] = go(index - 1, 0);
+	else
+		mem[index][last] = go(index - 1, 0) + go(index - 1, 1);
+
+	return mem[index][last];
+}
