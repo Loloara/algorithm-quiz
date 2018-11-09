3ce235fb2e6bf604dde0844bbfc786943901d1ae
loloara <addy94@naver.com>
2018-11-09 21:38:29 +1000

    11053 solved

---
+++ baekjoon/dp/11053.cpp
@@ -0,0 +1,39 @@
+//11053 가장 긴 증가하는 부분 수열 https://www.acmicpc.net/problem/11053
+#include <iostream>
+using namespace std;
+
+int A[1001];
+int mem[1001] = { 0 };
+
+int go(int index);
+
+int main() {
+	int N, num;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+		cin >> A[i];
+	mem[1] = 1;
+
+	go(N);
+
+	int max = 0;
+	for (int i = 1; i <= N; i++) 
+		if (max < mem[i]) max = mem[i];
+
+	cout << max;
+
+	return 0;
+}
+
+int go(int index) {
+	if (mem[index]) return mem[index];
+
+	for (int i = 1; i < index; i++) {
+		int val = go(i);
+		if (A[i] >= A[index]) continue;
+		if (mem[index] < mem[i]) mem[index] = mem[i];
+	}
+	mem[index] += 1;
+
+	return mem[index];
+}
