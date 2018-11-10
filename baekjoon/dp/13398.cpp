ecae0018fe6519d7a95f57f31658545ef06ef636
loloara <addy94@naver.com>
2018-11-10 20:22:41 +1000

    13398 solved by top-down dp

--- baekjoon/dp/13398.cpp
+++ baekjoon/dp/13398.cpp
@@ -1,9 +1,54 @@
 //13398 연속합2 https://www.acmicpc.net/problem/13398
-//for문으로 DP 구현
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
+int A[100001];
+int memL[100001];
+int memR[100001];
+int dpL(int index);
+int dpR(int index);
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> A[i];
+		memL[i] = -100000001;
+		memR[i] = -100000001;
+	}
+	memL[1] = A[1];
+	memR[N] = A[N];
+
+	dpL(N);
+	dpR(1);
+
+	int ans = memL[1];
+	for (int i = 2; i < N; i++) {
+		ans = max({ ans, memL[i - 1] + memR[i + 1],memL[i] });
+	}
+	if (ans < memL[N]) ans = memL[N];
+
+	cout << ans;
+
+	return 0;
+}
+
+int dpL(int index) {
+	if (memL[index] != -100000001) return memL[index];
+	memL[index] = max(A[index], A[index] + dpL(index - 1));
+
+	return memL[index];
+}
+
+int dpR(int index) {
+	if (memR[index] != -100000001) return memR[index];
+	memR[index] = max(A[index], A[index] + dpR(index + 1));
+
+	return memR[index];
+}
+
+/* for문으로 DP 구현
 int main() {
 	int N;
 	int A[100001];
@@ -23,7 +68,7 @@ int main() {
 		if (memL[i] < memL[i - 1] + A[i]) memL[i] = memL[i - 1] + A[i];
 		if (memR[j] < memR[j + 1] + A[j]) memR[j] = memR[j + 1] + A[j];
 	}
-	
+
 	int ans = memL[1];
 	for (int i = 2; i < N; i++) {
 		ans = max({ ans, memL[i - 1] + memR[i + 1],memL[i] });
@@ -34,3 +79,4 @@ int main() {
 
 	return 0;
 }
+*/
