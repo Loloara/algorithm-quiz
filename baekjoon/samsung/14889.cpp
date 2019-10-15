bd99c18632053170562f5e5eebf8eed66006b81d
loloara <addy94@naver.com>
2019-10-15 09:19:01 +0900

    14889 solved

--- baekjoon/samsung/14889.cpp
+++ baekjoon/samsung/14889.cpp
@@ -1,44 +1,51 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
-int main() {
-	int N;
-	int perm[21];
-	int board[21][21];
-	int min_v = 100;
+int N;
+int board[21][21];
+bool visited[21];
+int min_v = 100;
 
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
-			cin >> board[i][j];
-		perm[i] = i;
-	}
-
-	int facto = 1;
-	for (int i = N; i > 1; i--)
-		facto *= N;
-	facto /= 2;
-	int num = 1;
-	while(num++ != facto) {
+void dfs(int curr, int depth) {
+	if (depth == N / 2) {
+		vector<int> start, link;
 		int diff = 0;
-		for (int i = 0; i < (N / 2) - 1; i++) {
-			for (int j = i + 1; j < N / 2; j++) {
-				diff += board[perm[i]][perm[j]];
-				diff += board[perm[j]][perm[i]];
-			}
+
+		for (int i = 0; i < N; i++) {
+			if (visited[i])
+				start.push_back(i);
+			else
+				link.push_back(i);
 		}
 
-		for (int i = N / 2; i < N - 1; i++) {
-			for (int j = i + 1; j < N; j++) {
-				diff -= board[perm[i]][perm[j]];
-				diff -= board[perm[j]][perm[i]];
+		for (int i = 0; i < N / 2; i++) {
+			for (int j = i+1; j < N / 2; j++) {
+				diff += board[start[i]][start[j]];
+				diff += board[start[j]][start[i]];
+				diff -= board[link[i]][link[j]];
+				diff -= board[link[j]][link[i]];
 			}
 		}
 		if (diff < 0) diff *= -1;
 		min_v = min_v < diff ? min_v : diff;
-		next_permutation(perm, perm + N);
 	}
+	else {
+		for (int i = curr+1; i < N; i++) {
+			if (visited[i]) continue;
+			visited[i] = true;
+			dfs(i, depth + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> board[i][j];
+	dfs(-1, 0);
 	cout << min_v;
 
 	return 0;
