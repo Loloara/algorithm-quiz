d918395be42643766a0208fef2edbcfd4ebe5997
Jong-ku Lee <addy94@naver.com>
2020-11-13 17:14:52 +0900

    refactor prime number generator
    - 타겟 수의 제곱근 까지만 나눔 (그 이상의 수들 끼리의 곱으로 타겟 수를 만들 수 없다)
    - 나누는 수 제곱부터 소수 검사를 한다. (그 이하의 숫자는 이미 검사됨)

--- base/Prime_Number.cpp
+++ base/Prime_Number.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 bool isPrimeNumber(int a);
@@ -31,7 +32,7 @@ int main() {
 	return 0;
 }
 
-// BigO : ∑Á∆ÆN
+// BigO : Î£®Ìä∏N
 bool isPrimeNumber(int n) {
 	if (n < 2) return false;
 	for (int i = 2; i*i <= n; i++) {	//check integer number until root a
@@ -42,18 +43,18 @@ bool isPrimeNumber(int n) {
 }
 
 /* BigO : N log (log N)
-ø°∂Û≈‰Ω∫≈◊≥◊Ω∫¿« √º∂Û∞Ì ∫Œ∏£¥¬ πÊΩƒ¿∏∑Œ ¿€¿∫ º“ºˆ∫Œ≈Õ ±◊¿« πËºˆ∏¶ º“ºˆ »ƒ∫∏ø°º≠ πË¡¶«œ¥¬ πÊΩƒ
+ÏóêÎùºÌÜ†Ïä§ÌÖåÎÑ§Ïä§Ïùò Ï≤¥ÎùºÍ≥† Î∂ÄÎ•¥Îäî Î∞©ÏãùÏúºÎ°ú ÏûëÏùÄ ÏÜåÏàòÎ∂ÄÌÑ∞ Í∑∏Ïùò Î∞∞ÏàòÎ•º ÏÜåÏàò ÌõÑÎ≥¥ÏóêÏÑú Î∞∞Ï†úÌïòÎäî Î∞©Ïãù
 */
 bool* sieveOfEratosthenes(int n) {
 	bool* arr = new bool[n+1];
 	arr[0] = false;
 	arr[1] = false;
-	for (int i = 2; i < n + 1; i++)
+	for (int i = 2; i <= n; i++)
 		arr[i] = true;
 
-	for (int i = 2; i < n + 1; i++) {
+	for (int i = 2; i <= sqrt(n); i++) {
 		if (arr[i]) {
-			for (int j = i * 2; j < n + 1; j+=i) {	//i*2 ∫Œ∫–¿∫ ªÁΩ« i*i ∫Œ≈Õ ∞ÀªÁ«œ¥¬∞‘ ∏¬¿∏≥™ intπ¸¿ß∏¶ √ ∞˙«œ¥¬ ∞ÊøÏ∞° ª˝±‚π«∑Œ ¿Ã∑∏∞‘ ¿€º∫«—¥Ÿ.
+			for (int j = i * i; j <= n; j+=i) {
 				if (j%i == 0)
 					arr[j] = false;
 			}
